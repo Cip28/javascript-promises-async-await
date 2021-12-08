3259efd125109c8e849bb5a35bfeab023fbef880
Cip28 <ciprianbarbu38@yahoo.com>
2021-12-08 15:22:01 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -20,4 +20,4 @@ function getBooksOrMovies(){
 }
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
-getBooksOrMoviesPromise.then(results => console.log('getBooksOrMoviesPromise', results))
+getBooksOrMoviesPromise.then(results => {console.log('getBooksOrMoviesPromise', results)})
