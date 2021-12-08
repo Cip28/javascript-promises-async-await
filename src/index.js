47fcc3e69079ee3c2e7a676b62486d0026db4919
Cip28 <ciprianbarbu38@yahoo.com>
2021-12-08 15:21:00 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
-const movies = require('./movies.json');
+const movies = require('./data/movies.json');
 
 
 const getBooksAndMovies = () => {
-    Promise.all([fetchBooks(), fetchMovies()])
+    return Promise.all([fetchBooks(), fetchMovies()])
         .then(([books, movies]) => ({
             books,
             movies
@@ -14,7 +14,7 @@ const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => {console.log('getBooksAndMoviesPromise', results)})
 
 function getBooksOrMovies(){
-    Promise.race([fetchBooks(), fetchMovies()])
+    return Promise.race([fetchBooks(), fetchMovies()])
         .then(results => results)
         .catch(error => console.log("Error waiting for the promise race",error))
 }
