cdcbeae07e8f370c2eb2372d50af3d523f581d77
Cip28 <ciprianbarbu38@yahoo.com>
2021-12-08 15:10:07 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import {fetchWithTimeout} from './services';
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 
 
-const movies = require('./data/movies.json');
-export default function fetchMovies () {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000)
-        .then(resolveFunction)
+export default function getBooksAndMovies () {
+    Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ({
+            books,
+            movies
+        }))
+    .catch(error => console.log("Error in fetching the movies and books",error))
+
 }
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
-})
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => console.log('getBooksAndMoviesPromise', results))
+
+export default function getBooksOrMovies () {
+    Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log("Error waiting for the promise race",error))
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => console.log('getBooksOrMoviesPromise', results))
--- src/services.js
+++ src/services.js
@@ -3,3 +3,15 @@
 export default function fetchWithTimeout (delay) {
     return new Promise(resolve => setTimeout(resolve,delay))
 }
+export default function fetchMovies () {
+    return fetch("./data/movies.json")
+        .then(response => response.json())
+        .then(movies=>movies)
+        .catch(error=>console.log(error))
+}
+export default function fetchBooks () {
+    return fetch("./data/books.json")
+        .then(response => response.json())
+        .then(books=>books)
+        .catch(error=>console.log(error))
+}
