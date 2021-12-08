695a215aaa598d7a30606216757fc27a5a96efd7
Cip28 <ciprianbarbu38@yahoo.com>
2021-12-08 14:54:33 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import movies from 'movies.json'
+import fetchWithTimeout from './services';
+import './services/fetchWithTimeout'
+
 export default function fetchMovies () {
-    
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000)
+    .then(resolveFunction)
 }
+const moviePromise = fetchMovies();
+moviePromise.then
