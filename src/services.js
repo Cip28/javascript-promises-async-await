abe4215a4d5c3ffb23921f091909bd2e39a28c25
Cip28 <ciprianbarbu38@yahoo.com>
2021-12-09 09:27:46 +0200

    Completed module

--- src/services.js
+++ src/services.js
@@ -1,24 +1,24 @@
 
 
 export function fetchWithTimeout (delay) {
-    return new Promise(resolve => setTimeout(resolve,delay))
+    return new Promise(resolve => setTimeout(resolve, delay));
 }
 export function fetchMovies () {
     return fetch("./data/movies.json")
         .then(response => response.json())
-        .then(movies=>movies)
-        .catch(error=>console.log(error))
+        .then(movies => movies)
+        .catch(error => console.log(error));
 }
 export function fetchBooks () {
     return fetch("./data/books.json")
         .then(response => response.json())
-        .then(books=>books)
-        .catch(error=>console.log(error))
+        .then(books => books)
+        .catch(error => console.log(error));
 }
 export async function asyncFetchMovies(){
     try {
-        const response = await fetch("/data/movies.json")
-        const results = await response.json()
+        const response = await fetch("/data/movies.json");
+        const results = await response.json();
         return results;        
     }
     catch(error) {
@@ -27,8 +27,8 @@ export async function asyncFetchMovies(){
 } 
 export async function asyncFetchBooks(){
     try {
-        const response = await fetch("/data/books.json")
-        const results = await response.json()
+        const response = await fetch("/data/books.json");
+        const results = await response.json();
         return results;        
     }
     catch(error) {
